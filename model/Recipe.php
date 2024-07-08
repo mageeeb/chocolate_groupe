2f2ab470443e6789fa5794495299b8cc76956a4a
OnlyPassionCode <van.wassenhove.kevin.pro@hotmail.com>
2024-07-08 09:37:58 +0200

    user_id to user_name for comment

--- model/Recipe.php
+++ model/Recipe.php
@@ -78,10 +78,9 @@ class Recipe{
           GROUP_CONCAT(com.subject SEPARATOR '|||') AS subjects,
           GROUP_CONCAT(com.created_date SEPARATOR '|||') AS comments_created_dates,
           GROUP_CONCAT(com.stars SEPARATOR '|||') AS comments_stars,
-          GROUP_CONCAT(u.name SEPARATOR '|||') AS comments_username,
+          GROUP_CONCAT(com.user_name SEPARATOR '|||') AS comments_username,
           com.recipe_id AS com_recipe_id
         FROM `comment` com
-        LEFT JOIN `user` u ON com.user_id=u.id
         GROUP BY com.recipe_id
       ) com ON com.com_recipe_id=r.id
       WHERE id=$id;
