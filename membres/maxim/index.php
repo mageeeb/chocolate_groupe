2dd9d3c7d797b41d03db0225a03ae99033fbf9dd
Maxi Rojas <arnaldomrojas@outlook.com>
2024-07-09 16:40:36 +0200

    form

--- membres/maxim/index.php
+++ membres/maxim/index.php
@@ -2,6 +2,8 @@
 require_once "config.php";
 require_once "../../model/Recipe.php";
 
+
+
 $pdo = new PDO( DB_TYPE.":host=".DB_HOST.";dbname=".DB_NAME.";port=".DB_PORT.";charset=".DB_CHARSET,
 DB_LOGIN,
 DB_PWD);
